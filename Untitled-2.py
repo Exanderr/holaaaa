b6e1b83a308059d719d187e30afe007e5065ebfe
Exanderr <143761053+Exanderr@users.noreply.github.com>
2023-09-01 17:55:39 -0300

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -3,7 +3,7 @@ apellido = "Provati"
 curso = "4°5"
 tarjetan = "123123"
 dinero_en_cuenta = 150
-
+djjdlsakjldasjlkdasjkdsajlk = 150
 def mostrar_menu():
     print("--- MENÚ ---")
     print("a. Datos personales")
